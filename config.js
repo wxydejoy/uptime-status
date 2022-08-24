4245fd1f7bf11bf64f566452ff136c19baca6950
weiekko <81625961+wxydejoy@users.noreply.github.com>
2022-08-24 16:22:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: '站点状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur1392304-201dde6196e8d97f10cf9201'
   ],
 
   // 是否显示监测站点的链接
