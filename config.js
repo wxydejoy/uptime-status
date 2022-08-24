e4aa71b8256e673ca14ed9d49a06f577de2e9851
weiekko <81625961+wxydejoy@users.noreply.github.com>
2022-08-24 16:32:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ window.Config = {
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 30,
 
   // 导航栏菜单
   Navi: [
     {
       text: '项目主页',
-      url: 'https://github.com/geekyouth/uptime-status'
+      url: 'https://github.com/wxydejoy/uptime-status'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/geekyouth/'
+      url: 'https://github.com/wxydejoy/'
     },
     {
       text: '博客',
-      url: 'https://java666.cn'
+      url: 'https://c.undf.top'
     }
   ]
 };
